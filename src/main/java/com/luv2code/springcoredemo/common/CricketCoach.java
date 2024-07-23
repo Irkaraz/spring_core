7cd47bb5abc2f986538b459cbd7b09e57bd5a5e4
Jéssica Santos <jessica.js1306@gmail.com>
2024-07-23 11:49:27 -0300

    Java Config Bean

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -11,20 +11,6 @@ public class CricketCoach implements Coach{
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
 
-    //define our init method
-    @PostConstruct
-    public void doMyStartUpStuff(){
-        System.out.println("In doMyStartUpStuff: " + getClass().getSimpleName());
-
-    }
-
-    //define our destroy method
-    @PreDestroy
-    public void doMyCleanUpStuff(){
-        System.out.println("In doMyCleanUpStuff: " + getClass().getSimpleName());
-
-    }
-
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes!";
